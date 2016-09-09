3a8728b527bfca1bfb4290f13b0d03cd7dd220e6
Shinichiro Tomizono <sukuba@kate.nokuni.net>
2016-09-09 09:09:47 +0900

    not change directory

--- pdf-to-text.bat
+++ pdf-to-text.bat
@@ -1,3 +1,2 @@
 call python2.bat > NUL
-cd/D "%WINPYWORKDIR%"
 python pdf-to-text.py --normalize %*
--- word-to-text.bat
+++ word-to-text.bat
@@ -1,3 +1,2 @@
 call python3.bat > NUL
-cd/D "%WINPYWORKDIR%"
 python word-to-text.py --normalize %*
