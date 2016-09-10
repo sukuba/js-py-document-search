039a99f9fe0cbcab3a4d6a195b9fded7fc537d53
Shinichiro Tomizono <macomini@terigaki.local>
2016-09-10 19:24:47 +0900

    update ichitaro-to-text
    but it doesn't work

--- ichitaro-to-text.py
+++ ichitaro-to-text.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 """
-Extracts entire text from word document as utf-8 text file.
-Works only on Windows with Ms Word installed.
+Extracts entire text from Ichitaro document as utf-8 text file.
+Works only on Windows with Justsystem Ichitaro installed.
 Uses pywin32 to manipulate the Word application.
 Expects Python 3
 
@@ -29,11 +29,10 @@ class IchitaroApp(object):
             pythoncom.CoInitialize()
         self.app = win32com.client.DispatchEx('JXW.Application')
         self.app.Application.Visible = self.visible
-        #self.app.Application.DisplayAlerts = 0  # wdAlertsNone
         return self.app
         
     def __exit__(self, type, value, traceback):
-        self.app.Quit()
+        self.app.Quit
         del self.app
         if self.coinitialize:
             pythoncom.CoUninitialize()
@@ -49,23 +48,32 @@ class IchitaroDocument(object):
         
     def __enter__(self):
         print('Opening %s ...' % self.filename)
-        self.doc = self.app.Documents.Open(self.filename)
+        self.app.Documents.Open(self.filename)
+        """
+        Documents.Open doesn't return the Document.
+        so this function always return None.
+        And,
+        The above gives an automation error, 
+        so it doesn't work!
+        While in the other com server (VBA) can do it as same manner.
+        I gave up to dig this any more, 
+        because actually I do not own the Ichitaro application.
+        """
         return self.doc
         
     def __exit__(self, type, value, traceback):
-        #self.doc.Saved = True
-        self.doc.Close()
-        del self.doc
+        self.app.TaroLibrary.QuitDocumentWindow
         print('%s closed.' % self.filename)
     
 def get_entire_text(doc, app):
     """
     returns a text of whole document.
     """
-    lib =app.TaroLibrary
-    lib.SelectAll(1)
-    text = lib.GetString()
-    return text
+    app.TaroLibrary.SelectAll(1)
+    # select all text in the windows #1
+    texts = app.TaroLibrary.GetString()
+    # get selected text lines as a list
+    return '\n'.join(texts)
     
 def normal_text(text):
     """
@@ -95,34 +103,34 @@ def ichitaro_to_text(args):
 
 def main():
     r"""
-    ワード文書をutf-8 textファイルに変換する。
+    一太郎文書をutf-8 textファイルに変換する。
     
-    word-to-text.py E:\scratch\Hello.doc E:\scratch
-    word-to-text.py Hello.doc E:\scratch --normalize
+    ichitaro-to-text.py E:\scratch\Hello.jtd E:\scratch
+    ichitaro-to-text.py Hello.jtd E:\scratch --normalize
     
-    第1引数: 変換元ワード文書（フルパスまたは出力先パス）
+    第1引数: 変換元一太郎文書（フルパスまたは出力先パス）
     第2引数: 出力先ディレクトリ（フルパス）
     --normalize: テキストをUnicode正規化する
-    --invisible: ワードを非表示にする
+    --invisible: 一太郎を非表示にする
     --verbose: 冗長な情報を出力する
     
     utf-8 text ファイルを生成する。
-    ファイル名は末尾に .txt を付加する。 MyWord.doc -> MyWord.doc.txt
+    ファイル名は末尾に .txt を付加する。 MyWord.jtd -> MyWord.jtd.txt
     Unicode正規化には、jsngram packageを使用する。
     
     ワード本体が必要（インストール済みであること）。
     
-    このプログラムはワードを起動し、
+    このプログラムは一太郎を起動し、
     指定された文書ファイルを開き、
     変換したtextを保存し、
-    ワードを閉じる。
+    一太郎を閉じる。
     
     """
-    parser = argparse.ArgumentParser(description='ワード文書をutf-8 textファイルに変換する')
-    parser.add_argument('filename', help='ワード文書ファイル名(読み取り)')
+    parser = argparse.ArgumentParser(description='一太郎文書をutf-8 textファイルに変換する')
+    parser.add_argument('filename', help='一太郎文書ファイル名(読み取り)')
     parser.add_argument('dest', help='出力先ディレクトリ')
     parser.add_argument('-n', '--normalize', action='store_true', help='テキストをUnicode正規化する')
-    parser.add_argument('-i', '--invisible', action='store_true', help='ワードを非表示にする')
+    parser.add_argument('-i', '--invisible', action='store_true', help='一太郎を非表示にする')
     parser.add_argument('-v', '--verbose', action='store_true', help='冗長な情報を出力する')
     args = parser.parse_args()
     
