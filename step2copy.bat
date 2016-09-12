a34d34ca80314f68fc3c4cde74dba3464d4116d9
Shinichiro Tomizono <sukuba@kate.nokuni.net>
2016-09-13 08:39:15 +0900

    make bat safer

--- step2copy.bat
+++ step2copy.bat
@@ -2,8 +2,16 @@ rem ################################
 rem step2: データをコピーする
 rem ################################
 
+if "%ORIGINAL%" == "" goto TERMINATE
+if "%PUBLISH%" == "" goto TERMINATE
+
 @echo %ORIGINAL% から %PUBLISH%\pub\db にコピーします。
 
 robocopy "%ORIGINAL%" %PUBLISH%\pub\db /mir /xa:SHT
 
 rem # step2終了. ###
+exit /B
+
+:TERMINATE
+pause エラー: step1が正しく動いていないので、中止します。
+exit 1
